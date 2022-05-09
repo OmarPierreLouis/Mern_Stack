2254b353c657edee205ed7788732d08b957f1e99
Omar Pierre Louis <mizzy@Omars-MBP.fios-router.home>
2022-05-09 18:24:29 -0400

    Box Generator

---
+++ React/box-generator/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Box from './components/Box';
+
+function App() {
+  return (
+    <div className="App container">
+      <h1> Create Your Box Here</h1>
+      <Box></Box>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/box-generator/src/components/Box.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const Box = () => {
+    let [color, setColor] = useState("");
+    let [allBoxes, setAllBoxes] = useState([]);
+    
+    const addBox = (e)=> {
+        e.preventDefault();
+        let box = {color};
+        console.log("Our Color is ->", box)
+        setAllBoxes([...allBoxes,box])
+    }
+    return (
+        < >
+            <form onSubmit={addBox}>
+                <div className='form-group mt-5'>
+                    <label htmlFor=''></label>
+                    <input onChange={ (e)=>{setColor(e.target.value)}}type="text" className="form-control" placeholder='Enter Color Here' />
+                </div>
+                    <button className="btn btn-dark mb-5 mt-5" type="submit">Add Box</button>
+            </form>
+            <div className="flex-box">
+                {
+                    allBoxes.map((giveBox,idx)=>{
+                    return (
+                    <div key={idx} className= "box-plate" style={{backgroundColor:giveBox.color, color:"white"}}>
+                        {giveBox.color}
+                    </div>
+                    )
+                    })
+                }
+            </div>
+        </>
+    )
+}
+export default Box; 
